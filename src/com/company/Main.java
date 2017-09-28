96209592c9bd8dd7ed2806dda69e747d48787840
Yaroslav19 <galaolala@gmail.com>
2017-09-28 13:49:29 +0300

    add method print

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    public static void print(String[] args){
+        for(int i = 0; i < args.length; i++)
+        System.out.println(args[i]);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!!!");
+        args = new String[3];
+        Scanner read = new Scanner(System.in);
+        for(int i = 0; i < args.length; i++)
+            args[i] = read.nextLine();
+
+        print(args);
     }
 }
