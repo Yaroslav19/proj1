4c1106bba91505209c3f9dce3e36b0dd5d2bd627
Yaroslav19 <galaolala@gmail.com>
2017-09-28 13:56:55 +0300

    add method sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
+
+    public static void sort(String[] args) {
+        Arrays.sort(args, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+    }
+
 
     public static void print(String[] args){
         for(int i = 0; i < args.length; i++)
@@ -15,6 +27,7 @@ public class Main {
         for(int i = 0; i < args.length; i++)
             args[i] = read.nextLine();
 
+        sort(args);
         print(args);
     }
 }
